8aec16f2e6734a292e2c6e84918395ceb2fb2c20
Thea Deo <chanvuthea.eang@bi-kay.com>
2025-11-20 18:11:52 +0700

    add Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
 "use client";
 import React, { useEffect, useState } from "react";
-
-import axios from "axios";
 import PremiumScreen from "../screens/premiumScreen";
 import CreateLinkScreen from "@/screens/createLinkScreen";
+import { dataCouple } from "@/assets/data";
 
 //Prod
-const URL = "https://authentic-dream-ffddac2f07.strapiapp.com";
-const IMAGE_URL = "";
-const token =
-  "d2be169e601b94e8ddebf51813a30da113e429c06a878a38bf078d5d570351d851da8f71aebe71d1d805f05e22bdc9f3e71a7094b9f30cb2d8a2753e85387155e67d4ea16a1b753dc3720d3f8e4833e1c4427e33211ee9c912d0b8581b602eeca2fbc2751bf472eb6150d019c201365ed4768f3808d18c8435499f44b6ba516e";
 
-//dev
-// const URL = "http://localhost:1338";
-// const IMAGE_URL = "http://localhost:1338";
-// const token =
-//   "52b341e50262d91c67841881f3ce32b92f49063102e6b7d15185999d032728887562c2bb72a0dfaf3fa7f5147edcfe44230cc6529e90414e43e39e17a93eea90a931e39685c0aa1fa9b9aa4ab34a071a589b0c34004afbba96a2fc55523e31fee7bde5e7859ca4f2db268238ffc13aa06a9e1899227c144503cc81e44be43802";
+const IMAGE_URL = "";
 
 const App: React.FC = () => {
   const [coupleID, setCoupleID] = useState("");
@@ -29,39 +20,9 @@ const App: React.FC = () => {
   }, []);
 
   const [coupleData, setCoupleData] = useState<any>();
-  const [_, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const fetchAgendaData = async () => {
-      const baseEndpoint = `${URL}/api/couples?filters[couple_id][$eq]=${coupleID}`;
-      const populateFields = [
-        "background",
-        "floral_button_background",
-        "fong_logo",
-        "photo_booth",
-        "background_sound",
-      ];
-
-      let populateQuery = populateFields
-        .map((field) => `populate[${field}][fields]=url`)
-        .join("&");
-      const endpoint = `${baseEndpoint}&${populateQuery}`;
-
-      try {
-        const response = await axios.get(endpoint, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        setCoupleData(response?.data?.data[0]);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    coupleID !== "create" && fetchAgendaData();
+    coupleID !== "create" && setCoupleData(dataCouple);
   }, [coupleID]);
   const checkType = () => {
     switch (coupleData?.list_family_name?.type) {
---
+++ src/assets/data.js
@@ -0,0 +1,204 @@
+export const dataCouple = {
+  id: 75,
+  documentId: "atpbbr8ndd4on7l98eqpwmim",
+  couple_id: "ivnaim_so_eng20251228",
+  list_family_name: {
+    type: "premium",
+    bride: "អួន សុអេង",
+    groom: "ងីន អ៉ីវណាំ",
+    left_door:
+      "https://authentic-dream-ffddac2f07.media.strapiapp.com/left_door_4aa0764549.png",
+    background:
+      "https://authentic-dream-ffddac2f07.media.strapiapp.com/bakcground_me_c786756eb1.png",
+    right_door:
+      "https://authentic-dream-ffddac2f07.media.strapiapp.com/right_4e144fea05.png",
+    bride_father: "ឯម អួន",
+    bride_mother: "សន សាវ",
+    frame_flower:
+      "https://authentic-dream-ffddac2f07.media.strapiapp.com/flower_Frame_1_5f2ab18c51.png",
+    groom_father: "ឈុន តេកលីម",
+    groom_mother: "ហ៊ុន សុគាន់",
+  },
+  content_invitation:
+    "សម្តេច ទ្រង់ ឯកឧត្តម អ្នកឧកញ៉ា លោកឧកញ៉ា លោកជំទាវ លោក លោកស្រី អ្នកនាង កញ្ញា អញ្ជើញចូលរួមជាអធិបតីនិងជាភ្ញៀវកិត្តិយសដើម្បីប្រសិទ្ធពរជ័យ សិរីសួស្តីជ័យមង្គល ក្នុងពិធីរៀបអាពាហ៍ពិពាហ៍កូនប្រុស-កូនស្រី របស់យើងខ្ញុំ។",
+  content_location:
+    "និងពិសារភោជនាហារ ដែលនឺងប្រព្រឺត្តទៅនៅ ថ្ងៃអាទិត្យ ទី២៨ ខែធ្នូ ឆ្នាំ២០២៥ត្រូវនឺង ថ្ងៃ៩កើត ខែបុស្ស ឆ្នាំម្សាញ់ សប្តស័ក ព.ស ២៥៦៨ វេលាម៉ោង៥:០០ ល្ងាច ស្ថិតនៅគេហដ្ឋានខាងស្រី ភូមិវាល ឃុំខ្នុរដំបង ស្រុកជើងព្រៃ ខេត្តកំពង់ចាម ដោយមេត្រីភាព។",
+  content_agenda: {
+    date: {
+      countdown: "2025-12-28T23:59:59",
+      first_day: "២៧ ធ្នូ",
+      full_date: "ថ្ងៃអាទិត្យ ទី២៨ ខែធ្នូ ឆ្នាំ២០២៥",
+      second_day: "២៨ ធ្នូ",
+      short_date: "១២.២៨.២០២៥",
+    },
+    agendaList: {
+      first_day: [
+        {
+          time: "ម៉ោង ០៣:០០ រសៀល",
+          detail: "ពិធីសែនក្រុងពាលី",
+        },
+        {
+          time: "ម៉ោង ០៤:០០ រសៀល",
+          detail: "ពិធីសូត្រមន្តចម្រើនព្រះបរិត្ត",
+        },
+        {
+          time: "ម៉ោង ០៥:០០ ល្ងាច",
+          detail: "អញ្ជើញភ្ញៀវកិត្តិយសពិសាអាហារ",
+        },
+        {
+          time: "ម៉ោង ០៦:៣០ ល្ងាច",
+          detail: "ពិធីជុំពេលា និងជាវខាន់ស្លា",
+        },
+      ],
+      second_day: [
+        {
+          time: "ម៉ោង ០៦:៣០ ព្រឹក",
+          detail: "ជួបជុំភ្ញៀវកិត្តិយសរៀបចំហែរជំនូន",
+        },
+        {
+          time: "ម៉ោង ០៧:០០ ព្រឹក",
+          detail: "ពិធីហែរជំនូនកំណត់ចូលរោងជ័យ",
+        },
+        {
+          time: "ម៉ោង ០៧:៣០ ព្រឹក",
+          detail:
+            "ពិធីចៅមហានិយាយជើងការ សែនព្រេន រៀបរាប់ផ្លែឈើ និងពិសាភោជនាហារពេលព្រឹក",
+        },
+        {
+          time: "ម៉ោង ០៨:០០ ព្រឹក",
+          detail: "ពិធីបំពាក់ចិញ្ចៀន",
+        },
+        {
+          time: "ម៉ោង ០៩:០០ ព្រឹក",
+          detail: "ពិធីកាត់សក់បង្កក់សិរី កូនប្រុស-កូនស្រី",
+        },
+        {
+          time: "ម៉ោង ១១:០០ ព្រឹក",
+          detail: "ពិធីសំពះផ្ទឹមចងដៃ បង្វិលពពិល និងព្រះថោងតោងស្បៃ",
+        },
+        {
+          time: "ម៉ោង ១២:០០ ព្រឹក",
+          detail: "អញ្ជើញភ្ញៀវកិត្តិយសពិសាអាហារថ្ងៃត្រង់",
+        },
+        {
+          time: "ម៉ោង ០៥:០០ ល្ងាច",
+          detail: "ទទួលភ្ញៀវកិត្តិយសពិសាភោជនាហារ",
+        },
+      ],
+    },
+    khmer_date: "ត្រូវនឹង ថ្ងៃ១៤កើត ខែពិសាខឆ្នាំម្សាញ់ សប្តស័ក ព.ស ២៥៦៨",
+    calendar_link:
+      "https://calendar.google.com/calendar/render?action=TEMPLATE&text=សិរីមង្គលអាពាហ៍ពិពាហ៍ អ៉ីវណាំ និង សុអេង &dates=20251228/20251228&details=សូមគោរពអញ្ជើញ ឯកឧត្ដម អ្នកឧកញ៉ា លោកឧកញ៉ា លោកជំទាវ លោក លោកស្រី អ្នកនាងកញ្ញា អញ្ជើញចូលរួមជាអធិបតីយភាព និងជាភ្ញៀវកិត្តិយស ក្នុងកម្មវិធីសិរីមង្គលអាពាហ៍ពិពាហ៍ កូនប្រុស កូនស្រី របស់យើងខ្ញុំ។&location=https://maps.app.goo.gl/tDBEoSA9FG9msAxbA?g_st=atm",
+    event_location: {
+      link: "https://maps.app.goo.gl/tDBEoSA9FG9msAxbA?g_st=atm",
+      name: "ស្ថិតនៅគេហដ្ឋានខាងស្រី ភូមិវាល ឃុំខ្នុរដំបង ស្រុកជើងព្រៃ ខេត្តកំពង់ចាម ដោយមេត្រីភាព",
+    },
+    restaurant_location: {
+      link: "https://maps.app.goo.gl/tDBEoSA9FG9msAxbA?g_st=atm",
+      name: "ស្ថិតនៅគេហដ្ឋានខាងស្រី ភូមិវាល ឃុំខ្នុរដំបង ស្រុកជើងព្រៃ ខេត្តកំពង់ចាម ដោយមេត្រីភាព",
+    },
+  },
+  youtube_id: "nqt6-TVodA8",
+  content_thnakyou: {
+    wish: "យើងខ្ញុំសូមជូនពរ ឯកឧត្តម លោកឧកញ៉ា លោកជំទាវ លោក លោកស្រី អ្នកនាងកញ្ញា និងប្រិយមិត្តទាំងអស់សូមមានសុខភាពល្អ និងទទួលជោគជ័យគ្រប់ភារកិច្ចសូមអរគុណ!",
+    speech:
+      "យើងខ្ញុំជាមាតាបិតា កូនប្រុស កូនស្រី សូមអភ័យទោសដោយពុំបានជួបដោយផ្ទាល់និង ការសរសេរឈ្មោះរបស់ភ្ញៀវកិត្តិយសពុំបានត្រឹមត្រូវ ឬពុំបានសរសេរឈ្មោះ។",
+    thankyou:
+      "យើងខ្ញុំជាមាតាបិតា សូមថ្លែងអំណរគុណយ៉ាងជ្រាលជ្រៅចំពោះ ការអញ្ជើញចូលរួមជាភ្ញៀវកិត្តិយសដ៏ឧត្តុង្គឧត្តម ក្នុងពិធីរៀបអាពាហ៍ពិពាហ៍ កូនប្រុស កូនស្រី របស់យើងខ្ញុំ។",
+    last_speech:
+      "វត្តមានរបស់ ឯកឧត្តម លោកឧកញ៉ា លោកជំទាវ លោក លោកស្រី អ្នកនាងកញ្ញា និងប្រិយមិត្តទាំងអស់ គឺជាកិត្តិយសដ៏ឧត្តុង្គឧត្តមសម្រាប់គ្រួសាររបស់យើងខ្ញុំ។",
+  },
+  date: "26.08.2025",
+  createdAt: "2025-11-19T07:00:38.528Z",
+  updatedAt: "2025-11-20T10:45:50.974Z",
+  publishedAt: "2025-11-20T10:45:51.057Z",
+  background: null,
+  floral_button_background: {
+    id: 82,
+    documentId: "t1qd5tpb5lj19s8fqlfhwn1j",
+    url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/fong_logo2_1_175787fcc2.png",
+  },
+  fong_logo: null,
+  photo_booth: [
+    {
+      id: 58,
+      documentId: "obmbuchqoqpp2q8clj4uffbb",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/2_45121a51df.png",
+    },
+    {
+      id: 66,
+      documentId: "r3dyqo7rm2u7k6c95dnbutvv",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/6_a5a8717485.png",
+    },
+    {
+      id: 57,
+      documentId: "hw6wujatvpmmlrkcctd0l6pn",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/1_14ce5c4c10.png",
+    },
+    {
+      id: 63,
+      documentId: "g7v141nb9ghfdx0oce3ncs1m",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/5_df751d84b6.png",
+    },
+    {
+      id: 65,
+      documentId: "g6awag6hmss650cial662a39",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/8_85f8d997f9.png",
+    },
+    {
+      id: 59,
+      documentId: "tum3hi11jmp58j6jjcsd4pya",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/3_631dee25b0.png",
+    },
+    {
+      id: 84,
+      documentId: "hrr6r9c2c8xch8lcp1od5dan",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/725_A8824_copy_1_b2be269665.png",
+    },
+    {
+      id: 69,
+      documentId: "lov849xu8ero4rpo57mam911",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/725_A8913_copy_1_09e67cb2c3.png",
+    },
+    {
+      id: 72,
+      documentId: "vwfemtervlxy09crxha45lt5",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/DSC_02463_copy_1_a92346c559.png",
+    },
+    {
+      id: 85,
+      documentId: "w2xq3qbtuhyh887j0dgv05q1",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/725_A9491_copy_1_13686377f7.png",
+    },
+    {
+      id: 62,
+      documentId: "b2xmjfju5pwaowpilr6giypk",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/725_A9574_copy_3a8245ccb1.png",
+    },
+    {
+      id: 60,
+      documentId: "jeq043blhnu9ohlyagkh4ssj",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/725_A9389_copy_e946d03355.png",
+    },
+    {
+      id: 83,
+      documentId: "x9gxa6eubcga5ym4l4ek83dv",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/725_A9169_copy_1_7cf5c3dfe4.png",
+    },
+    {
+      id: 61,
+      documentId: "sdfdk65yel79sirfyd2kqlcd",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/4_bb04e97cd6.png",
+    },
+    {
+      id: 68,
+      documentId: "bghzhz5gylf9wup8zb5nckge",
+      url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/725_A9178_copy_1_0d790ca310.png",
+    },
+  ],
+  background_sound: {
+    id: 81,
+    documentId: "ucek0fdw46jmm4rcno2pgkac",
+    url: "https://authentic-dream-ffddac2f07.media.strapiapp.com/Noly_Records_Pre_Wedding_MV_mp3cut_net_1922c77112.mp3",
+  },
+};
